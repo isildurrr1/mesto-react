33356e61ef37ae699346b78539d0c4f6b553d3a6
Andrey Romashev <andreygaranin.0@yandex.ru>
2023-05-09 16:03:51 +0700

    return api.js

---
+++ src/utils/api.js
@@ -0,0 +1,90 @@
+class Api {
+  constructor(options) {
+    this.baseUrl = options.baseUrl;
+    this.headers = options.headers;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  getProfileInfo() {
+    return fetch(`${this.baseUrl}/users/me`, {
+      headers: this.headers
+    })
+    .then(this._checkResponse)
+  }
+
+  getInitialCards() {
+    return fetch(`${this.baseUrl}/cards`, {
+      headers: this.headers
+    })
+    .then(this._checkResponse)
+  }
+
+  editProfileInfo(data) {
+    return fetch(`${this.baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify({
+        name: `${data.name}`,
+        about: `${data.job}`
+      })
+    })
+    .then(this._checkResponse)
+  }
+
+  addNewCard(data) {
+    return fetch(`${this.baseUrl}/cards`, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify({
+        name: `${data.place}`,
+        link: `${data.src}`
+      })
+    })
+    .then(this._checkResponse)
+  }
+  deleteCard(cardId) {
+    return fetch(`${this.baseUrl}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this.headers,
+    })
+    .then(this._checkResponse)
+  }
+  likeCard(cardId) {
+    return fetch(`${this.baseUrl}/cards/${cardId}/likes`, {
+      method: 'PUT',
+      headers: this.headers,
+    })
+    .then(this._checkResponse)
+  }
+  deleteLikeCard(cardId) {
+    return fetch(`${this.baseUrl}/cards/${cardId}/likes`, {
+      method: 'DELETE',
+      headers: this.headers,
+    })
+    .then(this._checkResponse)
+  }
+  changeAvatar(image) {
+    return fetch(`${this.baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify({
+        avatar: `${image}`
+      })
+    })
+    .then(this._checkResponse)
+  }
+}
+
+export const api = new Api({
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-64',
+  headers: {
+    authorization: 'bbe8cda5-f620-40b5-b414-da957e140ed7',
+    'Content-Type': 'application/json'
+  }
+})
